8a86c4cf6ca2e59071db095fffc58316d0e838d5
TheOCJorsh <ogheghajoshua@gmail.com>
2025-12-01 18:35:10 +0100

    update to serial

--- files/serializers.py
+++ files/serializers.py
@@ -1,16 +1,8 @@
 from rest_framework import serializers
 from .models import File
-from rest_framework import serializers
-from django.conf import settings
 
 class FileSerializer(serializers.ModelSerializer):
-    share_link = serializers.SerializerMethodField()
-
     class Meta:
         model = File
-        fields = ['id', 'original_name', 'size', 'uploaded_at', 'share_link']
+        fields = ['id', 'owner', 'file', 'file_name', 'size', 'uploaded_at']
         read_only_fields = ['owner', 'uploaded_at']
-
-    def get_share_link(self, obj):
-        base_url = "https://cloudshare-production-eb41.up.railway.app"
-        return f"{base_url}/api/files/share/{obj.share_token}/"
